415043d36cddb23319410da992b992f742e1c6bf
shurintou <shurintou@gmail.com>
2022-04-20 04:26:01 +0900

    Fix(email.sh): add the condition that check the GIT_COMMITTER_NAME to make sure the shell won't overwrite other's commit records.

--- src/shell/gitChangeCommitEmail/email.sh
+++ src/shell/gitChangeCommitEmail/email.sh
@@ -2,22 +2,24 @@
 
 #place this file in the /src path, and then execute it.
 
-#Noat that the CORRECT_NAME is optional.
+#If there are any uncommitted change exist, this shell won't work. Please revert(or stash) the changes before the execution. 
+
+#[important!!!]
+# This shell is only recommended to individual project cause it may change other user's commits unintentionally,
+# so make sure the GIT_COMMITTER_NAME be checked and know what you about to do before the execution. 
 
 git filter-branch --env-filter '
 
+YOUR_USERNAME="yourUsername"
 OLD_EMAIL="wrong@email.com"
-CORRECT_NAME="correctUsername"
 CORRECT_EMAIL="correct@email.com"
 
-if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_COMMITTER_NAME" = "$YOUR_USERNAME" -a "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
 then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
 fi
-if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+if [ "$GIT_COMMITTER_NAME" = "$YOUR_USERNAME" -a "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
 then
-    export GIT_COMMITTER_NAME="$CORRECT_NAME"
     export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
 fi
 ' --tag-name-filter cat -- --branches --tags
